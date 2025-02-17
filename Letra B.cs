d053f2401871f2cb4591a811dd9c46f500c1cb38
Anna Rocha <168745681+Annacrocha@users.noreply.github.com>
2025-02-17 09:01:04 -0300

    algoritmo que leia os elementos de uma matriz inteira 10x10 e imprima os elementos da diagonal principal

--- Letra B.cs
+++ Letra B.cs
@@ -1,1 +1,30 @@
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int tamanho = 10;
+        int[,] matriz = new int[tamanho, tamanho];
 
+        Console.WriteLine("Insira os elementos da matriz 10x10");
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            for (int j = 0; j < tamanho; j++)
+            {
+                Console.WriteLine($"Elemento [{i + 1},{j + 1}] :");
+                matriz[i, j] = int.Parse(Console.ReadLine());
+
+            }
+        }
+
+        Console.WriteLine("\nElementos da Diagonal Principal:");
+        for (int i = 0; i < tamanho; i++)
+        {
+            Console.Write(matriz[i, i] + " ");
+        }
+
+        Console.WriteLine();
+        Console.Write("Press any key to continue . . . ");
+        Console.ReadKey(true);
+    }
+}
